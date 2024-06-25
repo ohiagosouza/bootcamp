20aa02c9b807ef2caef70b86ec41abc0273507b0
Hiago Souza <ohiagossouza@gmail.com>
2024-06-25 14:59:35 -0300

    add methods in Dev Class

--- src/com/hiago/bootcamp/domain/Dev.java
+++ src/com/hiago/bootcamp/domain/Dev.java
@@ -2,6 +2,7 @@ package com.hiago.bootcamp.domain;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,9 +10,27 @@ public class Dev {
   private Set<Content> subedContent = new LinkedHashSet<>();
   private Set<Content> doneContent = new LinkedHashSet<>();
 
-  public void subscribeToBootcamp(Bootcamp bootcamp){};
-  public void progress(){};
-  public void calcTotalXp(){}
+  public void subscribeToBootcamp(Bootcamp bootcamp){
+    this.subedContent.addAll(bootcamp.getContents());
+    bootcamp.getSubscribedDevs().add(this);
+  };
+
+  public void progress(){
+    Optional<Content> content = this.subedContent.stream().findFirst();
+    if(content.isPresent()){
+      this.doneContent.add(content.get());
+      this.subedContent.remove(content.get());
+    } else {
+      System.err.println("Você não está matriculado em nenhum conteúdo");
+    }
+  };
+
+  public double calcTotalXp(){
+    return this.doneContent
+                .stream()
+                .mapToDouble(Content::calculateXp)
+                .sum();
+  }
 
   @Override
   public boolean equals(Object o) {
